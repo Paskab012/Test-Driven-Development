1672635a526208a25db368c5c998970e2f8ae80a
usorfaitheloho <usorfaitheloho@gmail.com>
2022-05-27 20:16:30 -0700

    Create factorial and reverse method based on the factorial and reverse test done

---
+++ solver.rb
@@ -0,0 +1,20 @@
+class Solver
+    def factorial(num)
+      i = 1
+      fact = 1
+      raise RuntimeError if num.negative?
+  
+      while i <= num
+        fact *= i
+        i += 1
+      end
+  
+      fact
+    end
+  
+    def reverse(word)
+      word.reverse
+    end
+
+  end
+  
